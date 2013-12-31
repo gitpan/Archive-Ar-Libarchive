d0f965f45bc59adc42a9927143b7fa11e5657b47
Graham Ollis <plicease@cpan.org>
2013-12-31 17:32:39 +0000

    Import of PLICEASE/Archive-Ar-Libarchive-0.05 from CPAN.
    gitpan-cpan-distribution: Archive-Ar-Libarchive
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         PLICEASE/Archive-Ar-Libarchive-0.05.tar.gz
    gitpan-cpan-author:       PLICEASE
    gitpan-cpan-maturity:     released

--- lib/Archive/Ar/Libarchive.pm
+++ lib/Archive/Ar/Libarchive.pm
@@ -6,7 +6,7 @@ use Carp qw( carp );
 use File::Basename qw( basename );
 
 # ABSTRACT: Interface for manipulating ar archives with libarchive
-our $VERSION = '0.04'; # VERSION
+our $VERSION = '0.05'; # VERSION
 
 unless($^O eq 'MSWin32')
 {
@@ -211,7 +211,7 @@ Archive::Ar::Libarchive - Interface for manipulating ar archives with libarchive
 
 =head1 VERSION
 
-version 0.04
+version 0.05
 
 =head1 SYNOPSIS
 
